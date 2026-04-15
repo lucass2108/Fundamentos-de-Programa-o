8405f5115da3be451b85a642ca4842b780200e57
lucass2108 <correasilva2007@gmail.com>
2026-04-15 16:44:29 -0300

    ex01

--- AULA-TESTE/arrays/arrays.c
+++ AULA-TESTE/arrays/arrays.c
@@ -2,5 +2,30 @@
 
 int main()
 {
+    int numNotas;
+    scanf("%d", &numNotas);
+
+    float notas[numNotas], media;
+    media = 0;
+
+    for(int i=0; i < numNotas; i++)
+    {
+        scanf("%f", &notas[i]);
+    }
+
+    for(int i=0; i < numNotas; i++){
+        media += notas[i];
+    }
+
+    media /= numNotas;
+
+    for (int i = 0; i < numNotas; i++)
+    {
+        if (notas[i] > media)
+        {
+            printf("%.2f ", notas[i]);
+        }
+        
+    }
     
 }
