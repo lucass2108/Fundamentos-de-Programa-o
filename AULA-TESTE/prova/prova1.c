ded0fd726199ff5c5c95f0a7fa273fa3d9b02c5d
lucass2108 <correasilva2007@gmail.com>
2026-04-16 16:11:27 -0300

    ex06

--- AULA-TESTE/prova/prova1.c
+++ AULA-TESTE/prova/prova1.c
@@ -3,16 +3,19 @@
 
 int main()
 {
-    float num, soma, d=1.0;
-    scanf("%f", &num);
-
-    for(int i = 1.0; i <= num; i++)
+    float num, soma, i;
+    while (num >=0)
     {
-        soma += d/i;
-        d += 2.0;
+        scanf("%f", &num);
+        if (num >= 0)
+        {
+            soma += num;
+            i++;
+        }
     }
+
+    printf("%.2f", soma/i);
     
-    printf("%.3f\n", soma);
     
     return 0;
 }
