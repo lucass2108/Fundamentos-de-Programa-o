728997f3585741771f114c4400d19d936e69b290
lucass2108 <correasilva2007@gmail.com>
2026-04-16 15:48:30 -0300

    q01

--- AULA-TESTE/arrays/arrays.c
+++ AULA-TESTE/arrays/arrays.c
@@ -3,37 +3,16 @@
 
 int main()
 {   
-    float temp[6], maior, menor = 100000, media;
+    int n;
+    scanf("%d", &n);
 
-    for(int i=0; i<6; i++)
-    {
-        scanf("%f", &temp[i]);
+    float nums[n], soma;
+    for(int i = 0; i < n; i++) {
+        scanf("%f", &nums[i]);
+        soma += nums[i];
     }
 
-    for(int i = 0; i<6; i++)
-    {
-        if(temp[i] > maior)
-            maior = temp[i];
-        
-        if(temp[i] < menor)
-            menor = temp[i];
-
-        media += temp[i];
-    }
-
-    media = media / 6;
-
-    printf("%.2f %.2f %.2f\n", menor, maior, media);
-    if(media >= 18 && media <= 22)
-    {
-        printf("ok");
-    }
-    else
-    {
-        printf("nok");
-    }
-
-
+    printf("%.3f\n", soma);
 
 
     return 0;
---
+++ AULA-TESTE/prova/prova1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+
+    for(int i= num; i >0; i--)
+        printf("%d ", i);
+
+    return 0;
+}
