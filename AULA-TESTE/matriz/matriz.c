b326858573db0e4cad50d56cb863fc0b788b4f1b
lucass2108 <correasilva2007@gmail.com>
2026-04-23 16:26:35 -0300

    ex03Matriz

--- AULA-TESTE/matriz/matriz.c
+++ AULA-TESTE/matriz/matriz.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int n, k =0;
+    int n, k =1;
     scanf("%d", &n);
 
     int matriz[n][n];
@@ -11,6 +11,11 @@ int main()
     {
         for(int j = 0; j < n; j++)
         {
+            if (i > n-1)
+            {
+                break;
+            }
+            
             if(j == k)
             {
                 matriz[i][j] = 1;
